0c62ca3360aed6e98e168c992f64bc9af0143788
Tim Engel <engeltj@gmail.com>
2013-11-14 22:54:04 -0500

    Started WorldGuard+Edit functions for builders building their schematics

---
+++ src/com/tengel/time/WorldEditSelection.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.tengel.time;
+
+import com.sk89q.worldedit.Vector;
+import com.sk89q.worldedit.bukkit.selections.Selection;
+import com.sk89q.worldedit.regions.RegionSelector;
+import static java.lang.Math.abs;
+import org.bukkit.Location;
+import org.bukkit.World;
+
+/**
+ *
+ * @author Tim
+ */
+public class WorldEditSelection implements Selection {
+    private Vector nativeMinPoint,nativeMaxPoint;
+    private Location start,end;
+    
+    public WorldEditSelection(Location start, Location end){
+        nativeMinPoint = new Vector(start.getBlockX(),start.getBlockY(),start.getBlockZ());
+        nativeMinPoint = new Vector(end.getBlockX(),end.getBlockY(),end.getBlockZ());
+        this.start = start;
+        this.end = end;
+    }
+    
+    public Location getMinimumPoint() {
+        return start;
+    }
+
+    public Vector getNativeMinimumPoint() {
+        return nativeMinPoint;
+    }
+
+    public Location getMaximumPoint() {
+        return end;
+    }
+
+    public Vector getNativeMaximumPoint() {
+        return nativeMaxPoint;
+    }
+
+    public RegionSelector getRegionSelector() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public World getWorld() {
+        return start.getWorld();
+    }
+
+    public int getArea() {
+        int x = abs(start.getBlockX() - end.getBlockX())+1;
+        int y = abs(start.getBlockY() - end.getBlockY())+1;
+        int z = abs(start.getBlockZ() - end.getBlockZ())+1;
+        return x*y*z;
+    }
+
+    public int getWidth() {
+        return abs(start.getBlockX() - end.getBlockX())+1;
+    }
+
+    public int getHeight() {
+        return abs(start.getBlockY() - end.getBlockY())+1;
+    }
+
+    public int getLength() {
+        return abs(start.getBlockZ() - end.getBlockZ())+1;
+    }
+
+    public boolean contains(Location lctn) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/com/tengel/time/WorldGuardUtil.java
@@ -0,0 +1,350 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.tengel.time;
+
+
+import com.mysql.jdbc.log.Log;
+import com.sk89q.minecraft.util.commands.CommandContext;
+import com.sk89q.worldedit.CuboidClipboard;
+import com.sk89q.worldedit.EditSession;
+import com.sk89q.worldedit.LocalConfiguration;
+import com.sk89q.worldedit.LocalPlayer;
+import com.sk89q.worldedit.LocalSession;
+import com.sk89q.worldedit.LocalWorld;
+import com.sk89q.worldedit.ServerInterface;
+import com.sk89q.worldedit.Vector;
+import com.sk89q.worldedit.WorldEdit;
+import com.sk89q.worldedit.bukkit.BukkitCommandSender;
+import com.sk89q.worldedit.bukkit.BukkitPlayer;
+import com.sk89q.worldedit.bukkit.BukkitUtil;
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import com.sk89q.worldedit.bukkit.selections.Polygonal2DSelection;
+import com.sk89q.worldedit.bukkit.selections.Selection;
+import com.sk89q.worldedit.commands.SchematicCommands;
+import com.sk89q.worldedit.data.DataException;
+import com.sk89q.worldedit.regions.Region;
+import com.sk89q.worldedit.regions.RegionSelector;
+import com.sk89q.worldedit.schematic.SchematicFormat;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.domains.DefaultDomain;
+import com.sk89q.worldguard.protection.databases.ProtectionDatabaseException;
+import com.sk89q.worldguard.protection.flags.DefaultFlag;
+import com.sk89q.worldguard.protection.flags.Flag;
+import com.sk89q.worldguard.protection.flags.StateFlag;
+import com.sk89q.worldguard.protection.flags.StateFlag.State;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
+import com.sk89q.worldguard.protection.regions.ProtectedPolygonalRegion;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+/**
+ *
+ * @author Tim
+ */
+
+public class WorldGuardUtil {
+	public static WorldGuardPlugin wgp;
+	public static boolean hasWorldGuard = false;
+	static Map<String,Set<String>> trackedRegions = new ConcurrentHashMap<String,Set<String>>();
+
+	public static boolean hasWorldGuard() {
+		return WorldEditUtil.hasWorldEdit() && hasWorldGuard;
+	}
+        
+        public static ProtectedRegion updateProtectedRegion(String playerName, Location start, Location end) throws Exception {
+		return createRegion(playerName, start, end);
+	}
+
+	private static ProtectedRegion createRegion(String playerName, Location start, Location end)
+			throws ProtectionDatabaseException {
+            Selection sel = new WorldEditSelection(start,end);
+            String id = "buildplot_"+playerName;
+            World w = sel.getWorld();
+            RegionManager mgr = wgp.getGlobalRegionManager().get(w);
+            mgr.removeRegion(id);
+            ProtectedRegion region;
+                  // Detect the type of region from WorldEdit
+            if (sel instanceof Polygonal2DSelection) {
+                Polygonal2DSelection polySel = (Polygonal2DSelection) sel;
+                int minY = polySel.getNativeMinimumPoint().getBlockY();
+                int maxY = polySel.getNativeMaximumPoint().getBlockY();
+                region = new ProtectedPolygonalRegion(id, polySel.getNativePoints(), minY, maxY);
+            } else { /// default everything to cuboid
+                region = new ProtectedCuboidRegion(id,
+                            sel.getNativeMinimumPoint().toBlockVector(),
+                            sel.getNativeMaximumPoint().toBlockVector());
+            }
+            region.setPriority(11); /// some relatively high priority
+            //region.setFlag(DefaultFlag.PVP,State.ALLOW);
+            wgp.getRegionManager(w).addRegion(region);
+            mgr.save();
+            return region;
+	}
+
+
+	public static void clearRegion(ProtectedRegion pr, String world) {
+            World w = Bukkit.getWorld(world);
+            if (w==null)
+                    return;
+            if (pr == null)
+                    return;
+
+            Location l;
+            for (Item entity: w.getEntitiesByClass(Item.class)) {
+                    l = entity.getLocation();
+                    if (pr.contains(l.getBlockX(), l.getBlockY(), l.getBlockZ())){
+                            entity.remove();
+                    }
+            }
+	}
+        
+	public static boolean isLeavingArea(ProtectedRegion pr, final Location from, final Location to, final World w) {
+		if (pr == null)
+			return false;
+		return (!pr.contains(to.getBlockX(), to.getBlockY(), to.getBlockZ()) &&
+				pr.contains(from.getBlockX(), from.getBlockY(), from.getBlockZ()));
+	}
+
+	public static Flag<?> getWGFlag(String flagString){
+		for (Flag<?> f: DefaultFlag.getFlags()){
+			if (f.getName().equalsIgnoreCase(flagString)){
+				return f;
+			}
+		}
+		throw new IllegalStateException("Worldguard flag " + flagString +" not found");
+	}
+	public static StateFlag getStateFlag(String flagString){
+		for (Flag<?> f: DefaultFlag.getFlags()){
+			if (f.getName().equalsIgnoreCase(flagString) && f instanceof StateFlag){
+				return (StateFlag) f;
+			}
+		}
+		throw new IllegalStateException("Worldguard flag " + flagString +" not found");
+	}
+
+	public static boolean setFlag(ProtectedRegion pr, String worldName, String flag, boolean enable) {
+		World w = Bukkit.getWorld(worldName);
+		if (w == null)
+			return false;
+		if (pr == null)
+			return false;
+		StateFlag f = getStateFlag(flag);
+		State newState = enable ? State.ALLOW : State.DENY;
+		State state = pr.getFlag(f);
+
+		if (state == null || state != newState){
+			pr.setFlag(f, newState);}
+		return true;
+	}
+
+	public static boolean setWorldGuard(Plugin plugin) {
+		wgp = (WorldGuardPlugin) plugin;
+		hasWorldGuard = true;
+		return hasWorldGuard();
+	}
+
+	public static boolean allowEntry(ProtectedRegion pr, Player player, String regionWorld) {
+		World w = Bukkit.getWorld(regionWorld);
+		if (w == null)
+			return false;
+		if (pr == null)
+			return false;
+		DefaultDomain dd = pr.getMembers();
+		dd.addPlayer(player.getName());
+		pr.setMembers(dd);
+		return true;
+	}
+
+	public static boolean addMember(ProtectedRegion pr, String playerName, String regionWorld) {
+		return changeMember(pr,playerName,regionWorld,true);
+	}
+
+	public static boolean removeMember(ProtectedRegion pr, String playerName, String regionWorld) {
+		return changeMember(pr,playerName,regionWorld,false);
+	}
+
+	private static boolean changeMember(ProtectedRegion pr, String name, String regionWorld, boolean add){
+		World w = Bukkit.getWorld(regionWorld);
+		if (w == null)
+			return false;
+		if (pr == null)
+			return false;
+
+		DefaultDomain dd = pr.getMembers();
+		if (add){
+			dd.addPlayer(name);
+		} else {
+			dd.removePlayer(name);
+		}
+		pr.setMembers(dd);
+		return true;
+	}
+
+	public static void deleteRegion(String worldName, String id) {
+		World w = Bukkit.getWorld(worldName);
+		if (w == null)
+			return;
+		RegionManager mgr = wgp.getRegionManager(w);
+		if (mgr == null)
+			return;
+		mgr.removeRegion(id);
+	}
+
+	public static boolean pasteSchematic(CommandSender sender, ProtectedRegion pr, String schematic, World world) {
+		CommandContext cc = null;
+		String args[] = {"load", schematic};
+		final WorldEditPlugin wep = WorldEditUtil.getWorldEditPlugin();
+		final WorldEdit we = wep.getWorldEdit();
+		LocalPlayer bcs = new ConsolePlayer(wep,wep.getServerInterface(),sender, world);
+
+		final LocalSession session = wep.getWorldEdit().getSession(bcs);
+		session.setUseInventory(false);
+		EditSession editSession = session.createEditSession(bcs);
+		Vector pos = new Vector(pr.getMinimumPoint());
+		try {
+			cc = new CommandContext(args);
+			return loadAndPaste(cc, we, session, bcs,editSession,pos);
+		} catch (Exception e) {
+                        
+			//Log.printStackTrace(e);
+			return false;
+		}
+	}
+
+	public static class ConsolePlayer extends BukkitCommandSender {
+		LocalWorld world;
+		public ConsolePlayer(WorldEditPlugin plugin, ServerInterface server,CommandSender sender, World w) {
+			super(plugin, server, sender);
+			world = BukkitUtil.getLocalWorld(w);
+		}
+
+		@Override
+		public boolean isPlayer() {
+			return true;
+		}
+		@Override
+		public LocalWorld getWorld() {
+			return world;
+		}
+	}
+        
+	public static boolean saveSchematic(Player p, String schematicName){
+            CommandContext cc = null;
+            WorldEditPlugin wep = WorldEditUtil.getWorldEditPlugin();
+            final LocalSession session = wep.getSession(p);
+            final BukkitPlayer lPlayer = wep.wrapPlayer(p);
+            EditSession editSession = session.createEditSession(lPlayer);
+
+            try {
+                    Region region = session.getSelection(lPlayer.getWorld());
+                    Vector min = region.getMinimumPoint();
+                    Vector max = region.getMaximumPoint();
+                    CuboidClipboard clipboard = new CuboidClipboard(
+                                    max.subtract(min).add(new Vector(1, 1, 1)),
+                                    min, new Vector(0,0,0));
+                    clipboard.copy(editSession);
+                    session.setClipboard(clipboard);
+
+                    SchematicCommands sc = new SchematicCommands(wep.getWorldEdit());
+                    String args2[] = {"save", "mcedit", schematicName};
+                    cc = new CommandContext(args2);
+                    sc.save(cc, session, lPlayer, editSession);
+                    return true;
+            } catch (Exception e) {
+                    //Log.printStackTrace(e);
+                    return false;
+            }
+	}
+
+	/**
+	 * This is just copied and pasted from world edit source, with small changes to also paste
+	 * @param args
+	 * @param we
+	 * @param session
+	 * @param player
+	 * @param editSession
+	 */
+	public static boolean loadAndPaste(CommandContext args, WorldEdit we,
+			LocalSession session, com.sk89q.worldedit.LocalPlayer player, EditSession editSession, Vector pos) {
+
+		LocalConfiguration config = we.getConfiguration();
+
+		String filename = args.getString(0);
+		File dir = we.getWorkingDirectoryFile(config.saveDir);
+		File f = null;
+		try {
+			f = we.getSafeOpenFile(player, dir, filename, "schematic",new String[] {"schematic"});
+			String filePath = f.getCanonicalPath();
+			String dirPath = dir.getCanonicalPath();
+
+			if (!filePath.substring(0, dirPath.length()).equals(dirPath)) {
+				printError(player,"Schematic could not read or it does not exist.");
+				return false;
+			}
+			SchematicFormat format = SchematicFormat.getFormat(f);
+			if (format == null) {
+				printError(player,"Unknown schematic format for file" + f);
+				return false;
+			}
+
+			if (!filePath.substring(0, dirPath.length()).equals(dirPath)) {
+				printError(player,"Schematic could not read or it does not exist.");
+			} else {
+				session.setClipboard(format.load(f));
+				//				WorldEdit.logger.info(player.getName() + " loaded " + filePath);
+				//				print(player,filePath + " loaded");
+			}
+			session.getClipboard().paste(editSession, pos, false, true);
+			//			WorldEdit.logger.info(player.getName() + " pasted schematic" + filePath +"  at " + pos);
+		} catch (DataException e) {
+			printError(player,"Load error: " + e.getMessage());
+		} catch (IOException e) {
+			printError(player,"Schematic could not read or it does not exist: " + e.getMessage());
+		} catch (Exception e){
+			//Log.printStackTrace(e);
+			printError(player,"Error : " + e.getMessage());
+		}
+		return true;
+	}
+
+	private static void printError(LocalPlayer player, String msg){
+		if (player == null){
+			//Log.err(msg);
+		} else {
+			player.printError(msg);
+		}
+	}
+
+	public static boolean contains(Location location, ProtectedRegion pr) {
+		if (pr == null)
+			return false;
+		return pr.contains(location.getBlockX(), location.getBlockY(), location.getBlockZ());
+	}
+
+	public static boolean hasPlayer(String playerName, ProtectedRegion pr) {
+		if (pr == null)
+			return true;
+		DefaultDomain dd = pr.getMembers();
+		if (dd.contains(playerName))
+			return true;
+		dd = pr.getOwners();
+		return dd.contains(playerName);
+	}
+
+}
